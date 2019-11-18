08ed76106bf5e4c4f923d643c66dfbc5d55f62f9
haiyanx <haiyanxuu@gmail.com>
2019-11-18 18:19:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 setup(
-    name='imagestore',
-    version='3.2.0',
+    name='imagestore-mitoc',
+    version='4.0.0',
     packages=find_packages(),
     install_requires=[
         'django>=1.11',
@@ -17,12 +17,12 @@ setup(
         'swapper',
         'chardet',
     ],
-    author='Pavel Zhukov',
-    author_email='gelios@gmail.com',
-    description='Gallery solution for django projects',
+    author='Haiyan Xu',
+    author_email='haiyanxuu@gmail.com',
+    description='forked from hovel/imagestore... imagestore version for use with MITOC photo gallery',
     long_description=README,
     license='GPL',
     keywords='django gallery',
-    url='https://github.com/hovel/imagestore',
+    url='https://github.com/haiyanxu/imagestore-mitoc',
     include_package_data=True
 )
