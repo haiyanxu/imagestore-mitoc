357553d2bc642dcdd0b06238d62fe99ecd4c234d
haiyanx <haiyanxuu@gmail.com>
2019-12-20 14:19:44 -0500

    Get secret key from environment variable

--- test/testproject/testproject/settings.py
+++ test/testproject/testproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'om$sj2g=(bl312zxv_(eh&g_8ulevlba+v11wp@m%k&h1vianp'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
